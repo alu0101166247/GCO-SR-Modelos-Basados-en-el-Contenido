dbf281f1fedc2433e464cd9fb2a15cb8e2538f57
alu0101166247 <alu0101166247@ull.edu.es>
2021-12-16 16:56:08 +0000

    Base 2

--- js/data.js
+++ js/data.js
@@ -1,293 +1,94 @@
-class Person {
-  constructor(person, matrix, metrica) {
-    this.person = person;
-    this.matrix = matrix;
-    this.matrizCoor = [];
-    this.promedio = this.promedio();
-    if (metrica == "1.1") { // Correlacion de Pearson
-      this.setMatrizCorrZero();
-      this.calcPearson();
-    }
-    if (metrica == "1.2") { // Distancia Coseno
-      this.setMatrizCorrZero();
-      this.calcCoseno();
-    }
-    if (metrica == "1.3") { // Distancia Euclidea
-      this.setMatrizCorrZero();
-      this.calcEuclidea();
-    }
-  }
-  get getPerson() {
-    return this.person;
-  }
-  get getPromedio() {
-    return this.promedio;
-  }
-  promedio() {
-    let n = 0;
-    let out = 0;
-    for (y in this.matrix[this.person]) {
-      if (parseInt(this.matrix[this.person][y])) {
-        n++;
-        out += parseInt(this.matrix[this.person][y]);
-      }
-    }
-    return out / n;
-  }
-  correlacion(persona) {
-    return this.matrizCoor[this.person][persona];
-  }
-  setMatrizCorrZero() {
-    for (let x = 0; x < this.matrix.length; x++) {
-      this.matrizCoor[x] = new Array(this.matrix.length);
-    }
-    for (let x = 0; x < this.matrix.length; x++) {
-      for (let y = 0; y < this.matrix.length; y++) {
-        this.matrizCoor[x][y] = 0;
-      }
-    }
-  }
-  calcPearson() {
-    let outS = "";
-    out += `\rSimilaridad:\r`;
-    for (x in this.matrix) {
-      let correlacion = 0;
-      var sx = 0; var sy = 0; var sxy = 0; var sx2 = 0; var sy2 = 0; var n = 0;
-      if (parseInt(x) != parseInt(this.person)) {
-        for (y in this.matrix[x]) {
-          if (parseInt(this.matrix[this.person][y]) && parseInt(this.matrix[x][y])) {
-            var xx = parseFloat(this.matrix[this.person][y]);
-            var yy = parseFloat(this.matrix[x][y]);
-            n++;
-            sx = sx + xx; sy = sy + yy;
-            sx2 = sx2 + xx * xx; sy2 = sy2 + yy * yy;
-            sxy = sxy + xx * yy;
-            var mx = (sx / n);
-            var my = (sy / n);
-            var dtx = Math.sqrt(sx2 / n - mx * mx);
-            var dty = Math.sqrt(sy2 / n - my * my);
-            var cov = (sxy / n - mx * my);
-            correlacion = (cov / (dtx * dty)).toFixed(2);
-          }
-        }
-        this.matrizCoor[this.person][x] = correlacion;
-        outS = `sim(Person${this.person}, Person${x}) = ${correlacion}\r`;
-        out += outS;
-      }
-    }
-  }
-  calcCoseno() {
-    let outS = "";
-    out += `\rSimilaridad:\r`;
-    for (x in this.matrix) {
-      let correlacion = 0;
-      var sx = 0; var sy = 0; var sxy = 0; var sx2 = 0; var sy2 = 0;
-      if (parseInt(x) != parseInt(this.person)) {
-        for (y in this.matrix[x]) {
-          if (parseInt(this.matrix[this.person][y]) && parseInt(this.matrix[x][y])) {
-            var xx = parseFloat(this.matrix[this.person][y]);
-            var yy = parseFloat(this.matrix[x][y]);
-            sx = sx + xx; sy = sy + yy;
-            sx2 = sx2 + xx * xx; sy2 = sy2 + yy * yy;
-            sxy = sxy + xx * yy;
-            var dtx = Math.sqrt(sx2);
-            var dty = Math.sqrt(sy2);
-            var cov = sxy;
-            correlacion = (cov / (dtx * dty)).toFixed(2);
-          }
-        }
-        this.matrizCoor[this.person][x] = correlacion;
-        outS = `sim(Person${this.person}, Person${x}) = ${correlacion}\r`;
-        out += outS;
-      }
-    }
-  }
-  calcEuclidea() {
-    let outS = "";
-    out += `\rSimilaridad:\r`;
-    for (x in this.matrix) {
-      let correlacion = 0;
-      var sx = 0; var sy = 0; var sxy = 0; var sx2 = 0; var sy2 = 0;
-      if (parseInt(x) != parseInt(this.person)) {
-        for (y in this.matrix[x]) {
-          if (parseInt(this.matrix[this.person][y]) && parseInt(this.matrix[x][y])) {
-            var xx = parseFloat(this.matrix[this.person][y]);
-            var yy = parseFloat(this.matrix[x][y]);
-            sxy = sxy + xx - yy;
-            var cov = sxy * sxy;
-            correlacion = Math.sqrt(cov).toFixed(2);
-          }
-        }
-        this.matrizCoor[this.person][x] = correlacion;
-        outS = `sim(Person${this.person}, Person${x}) = ${correlacion}\r`;
-        out += outS;
-      }
-    }
-  }
-}
-
 let myMatrix = [];
-let currentPerson;
-let out = "";
 
-function guiones() {
-  for (a in myMatrix) {
-    for (b in myMatrix[a]) {
-      if (myMatrix[a][b] == '-') {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+function generarTablas() {
+  let salidahtml = document.getElementById('tablas');
+  
+  let table = document.createElement('table');
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
 
-function vecinosProximos(num, currentPerson, euclidea) {
-  let outF = [];
-  out += `\rVecinos seleccionados:\r`;
-  let outV = "";
-  let array = [];
-  for (x in currentPerson.matrizCoor[currentPerson.person]) {
-    const dupla = { vecino: x, valor: currentPerson.matrizCoor[currentPerson.person][x] };
-    array.push(dupla);
-  }
-  if (euclidea == "1.3") {
-    array.sort(function (a, b) {
-      return a.valor - b.valor;
-    });
-  } else {
-    array.sort(function (a, b) {
-      return b.valor - a.valor;
-    });
-  }
-  for (let i = 0; i < num; i++) {
-    if (array[i].vecino != currentPerson.getPerson) {
-      outF.push(array[i].vecino);
-      outV = `Vecino ${array[i].vecino}\r`;
-      out += outV;
-    } else {
-      num++;
-    }
-  }
-  return outF;
-}
+  table.appendChild(thead);
+  table.appendChild(tbody);
 
-function promedioPersona(matrix, vecino) {
-  let n = 0;
-  let out = 0;
-  for (y in matrix[vecino]) {
-    if (parseInt(matrix[vecino][y])) {
-      n++;
-      out += parseInt(matrix[vecino][y]);
-    }
-  }
-  return out / n;
-}
+  // Adding the entire table to the body tag
+  salidahtml.appendChild(table);
 
-function predecir() {
-  let outM = "";
-  let outI = "";
-  var salidahtml = document.getElementById('matrizNueva');
-  var num = document.getElementById('num_vecinos').value;
-  var metrica = document.getElementById('metrica').value;
-  var prediccion = document.getElementById('prediccion').value;
-  if (metrica == 1 || prediccion == 2 || num == '') {
-    salidahtml.innerHTML = "Faltan parametros por especificar";
-  } else {
-    if (num >= myMatrix.length || num == 0) {
-      salidahtml.innerHTML = "Numero de vecinos incorrecto";
-    } else {
-      let button = document.querySelector(".button");
-      while (guiones()) {
-        for (x in myMatrix) {
-          for (w in myMatrix[x]) {
-            if (myMatrix[x][w] == '-') {
-              currentPerson = new Person(x, myMatrix, metrica);
-              if (prediccion == "2.1") { // Prediccion simple
-                let item;
-                let top = 0;
-                let bot = 0;
-                let vecinos = vecinosProximos(num, currentPerson, metrica);
-                for (let i = 0; i < vecinos.length; i++) {
-                  top += currentPerson.matrizCoor[currentPerson.getPerson][vecinos[i]] * myMatrix[vecinos[i]][w];
-                }
-                for (let i = 0; i < vecinos.length; i++) {
-                  bot += parseFloat(currentPerson.matrizCoor[currentPerson.getPerson][vecinos[i]]);
-                }
-                item = (top / bot).toFixed(2);
-                if (isNaN(item) || item <= 0 || item > 5) {
-                  item = currentPerson.getPromedio.toFixed(2);
-                  myMatrix[currentPerson.getPerson][w] = item;
-                } else {
-                  myMatrix[currentPerson.getPerson][w] = item;
-                }
-                out += `\rCalculo de cada prediccion:\r`;
-                outI = `rˆ(Person${currentPerson.getPerson}, Item${parseInt(w) + 1}) = ${item}\r`;
-                out += outI;
-              } else { //Diferencia con la media
-                let item;
-                let top = 0;
-                let bot = 0;
-                let vecinos = vecinosProximos(num, currentPerson, metrica);
-                for (let i = 0; i < vecinos.length; i++) {
-                  top += (currentPerson.matrizCoor[currentPerson.getPerson][vecinos[i]] * (myMatrix[vecinos[i]][w] - promedioPersona(myMatrix, vecinos[i])));
-                }
-                for (let i = 0; i < vecinos.length; i++) {
-                  bot += parseFloat(currentPerson.matrizCoor[currentPerson.getPerson][vecinos[i]]);
-                }
-                item = (currentPerson.getPromedio + top / bot).toFixed(2);
-                if (isNaN(item)) {
-                  item = currentPerson.getPromedio.toFixed(2);
-                  myMatrix[currentPerson.getPerson][w] = item;
-                } else {
-                  myMatrix[currentPerson.getPerson][w] = item;
-                }
-                out += `\rCalculo de cada prediccion:\r`;
-                outI = `rˆ(Person${currentPerson.getPerson}, Item${parseInt(w) + 1}) = ${item}\r`;
-                out += outI;
-              }
-            }
-          }
-        }
-      }
-      //Salida
-      for (x in myMatrix) {
-        for (y in myMatrix[x]) {
-          outM += myMatrix[x][y] + " ";
-        }
-        outM += '\r';
-      }
-      salidahtml.innerHTML = `Matriz de utilidad:\r` + outM + out;
-      button.disabled = true;
-    }
-  }
+  // Creating and adding data to first row of the table
+  let row_1 = document.createElement('tr');
+  let heading_1 = document.createElement('th');
+  heading_1.innerHTML = "Sr. No.";
+  let heading_2 = document.createElement('th');
+  heading_2.innerHTML = "Name";
+  let heading_3 = document.createElement('th');
+  heading_3.innerHTML = "Company";
+
+  row_1.appendChild(heading_1);
+  row_1.appendChild(heading_2);
+  row_1.appendChild(heading_3);
+  thead.appendChild(row_1);
+
+
+  // Creating and adding data to second row of the table
+  let row_2 = document.createElement('tr');
+  let row_2_data_1 = document.createElement('td');
+  row_2_data_1.innerHTML = "1.";
+  let row_2_data_2 = document.createElement('td');
+  row_2_data_2.innerHTML = "James Clerk";
+  let row_2_data_3 = document.createElement('td');
+  row_2_data_3.innerHTML = "Netflix";
+
+  row_2.appendChild(row_2_data_1);
+  row_2.appendChild(row_2_data_2);
+  row_2.appendChild(row_2_data_3);
+  tbody.appendChild(row_2);
+
+
+  // Creating and adding data to third row of the table
+  let row_3 = document.createElement('tr');
+  let row_3_data_1 = document.createElement('td');
+  row_3_data_1.innerHTML = "2.";
+  let row_3_data_2 = document.createElement('td');
+  row_3_data_2.innerHTML = "Adam White";
+  let row_3_data_3 = document.createElement('td');
+  row_3_data_3.innerHTML = "Microsoft";
+
+  row_3.appendChild(row_3_data_1);
+  row_3.appendChild(row_3_data_2);
+  row_3.appendChild(row_3_data_3);
+  tbody.appendChild(row_3);
+
+  let button = document.querySelector(".button");
+  button.disabled = true;
 }
 
 function read(input) {
   let button = document.querySelector(".button");
   button.disabled = false;
   if (input.files && input.files[0]) {
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onload = function (e) {
-      var contents = e.target.result;
+      let contents = e.target.result;
       myMatrix = contents.split("\n");
+      mostrarDocumentos(myMatrix);
       for (x in myMatrix) {
         myMatrix[x] = myMatrix[x].replace('\r', '');
+        myMatrix[x] = myMatrix[x].replace(/\,/g, '');
+        myMatrix[x] = myMatrix[x].replace(/\./g, '');
         myMatrix[x] = myMatrix[x].split(" ");
       }
-      mostrarMatrizOri(myMatrix);
+      console.log(myMatrix);
     };
     reader.readAsText(input.files[0]);
   }
   out = "";
 }
 
-function mostrarMatrizOri(matriz) {
-  console.log(myMatrix);
+function mostrarDocumentos(matriz) {
   let out = "";
-  var elemento = document.getElementById('matrizOriginal');
+  let elemento = document.getElementById('documentos');
   for (x in matriz) {
     for (y in matriz[x]) {
-      out += matriz[x][y] + " ";
+      out += matriz[x][y];
     }
     out += '\r';
   }
